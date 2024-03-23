5b75baabc9fd40a17ac67e2787b7168b2ef16954
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-03-23 17:41:53 +0300

    first commit

---
+++ app/client/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Client() {
+  const [data, setData] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://icanhazdadjoke.com/", {
+        headers: { accept: "application/json" },
+      });
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div>{data.joke}</div>
+      <Link href="/server">go to server</Link>
+    </>
+  );
+}
